773fa8be31b8a7fa22ba6acd526379d61fb4c2fb
SzymonPobiega <szymon.pobiega@gmail.com>
2017-12-08 08:57:02 +0100

    Removed unnecessary persistence config

--- src/AcceptanceTests/AcceptanceTests/EndpointTemplates/ConfigureExtensions.cs
+++ src/AcceptanceTests/AcceptanceTests/EndpointTemplates/ConfigureExtensions.cs
@@ -13,13 +13,6 @@
             runDescriptor.OnTestCompleted(_ => transportConfiguration.Cleanup());
         }
 
-        public static async Task DefinePersistence(this EndpointConfiguration config, RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration)
-        {
-            var persistenceConfiguration = new ConfigureEndpointLearningPersistence();
-            await persistenceConfiguration.Configure(endpointCustomizationConfiguration.EndpointName, config, runDescriptor.Settings, endpointCustomizationConfiguration.PublisherMetadata);
-            runDescriptor.OnTestCompleted(_ => persistenceConfiguration.Cleanup());
-        }
-
         public static void RegisterComponentsAndInheritanceHierarchy(this EndpointConfiguration builder, RunDescriptor runDescriptor)
         {
             builder.RegisterComponents(r => { RegisterInheritanceHierarchyOfContextOnContainer(runDescriptor, r); });
