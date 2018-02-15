104455529115734aac6e18c23caf9f27c5e36e65
danielmarbach <daniel.marbach@openplace.net>
2018-02-15 09:24:37 +0100

    Reproduction unit tests for settings after customization

---
+++ src/ServiceControl.TransportAdapter.Tests/ControlForwarderTests.cs
@@ -0,0 +1,31 @@
+﻿namespace ServiceControl.TransportAdapter.Tests
+{
+    using NServiceBus.Configuration.AdvanceExtensibility;
+    using NServiceBus.Settings;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ControlForwarderTests
+    {
+        [Test]
+        public void Should_apply_standard_config_before_customizations()
+        {
+            const string CreateQueuesKey = "NServiceBus.Raw.CreateQueue";
+
+            SettingsHolder frontEndSettings = null, backendSettings = null;
+            // ReSharper disable once UnusedVariable
+            var controlForwarder = new ControlForwarder<FakeTransport, FakeTransport>("adapterName", "frontendControlQueue", "backendControlQueue", "poisonMessageQueue", frontend =>
+            {
+                frontEndSettings = frontend.GetSettings();
+                frontEndSettings.Set(CreateQueuesKey, false);
+            }, backend =>
+            {
+                backendSettings = backend.GetSettings();
+                backendSettings.Set(CreateQueuesKey, false);
+            }, 0);
+
+            Assert.IsFalse(frontEndSettings.Get<bool>(CreateQueuesKey));
+            Assert.IsFalse(backendSettings.Get<bool>(CreateQueuesKey));
+        }
+    }
+}
